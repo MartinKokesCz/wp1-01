35161867e664ebc9cdba3e442d3e5a215bf86d81
Martin Kokeš <info@martinkokes.cz>
2018-03-08 14:56:23 +0100

    pridani index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Nadpis</h1>
+    <?php
+      echo 'ahoj já jsem Martin Kokeš';
+      $age = 30;
+      $firstname = "Martin";
+      // příjmení
+      // základní škola
+      echo $age;
+    ?>
+    <p>toto je můj první php skript</p>
+
+    <p>Jméno: <?php echo $firstname; ?></p>
+    <p>Příjmení: </p>
+    <p>Věk:</p>
+    <p>Vzdělání:<?php  ?></p>
+
+  </body>
+</html>
